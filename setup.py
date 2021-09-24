b4b435c3712a95d2413bfb2ff682b6c33e98c702
mpappas <mano.pappa@gmail.com>
2021-09-24 12:00:12 +0100

    APIRTC-77 Remove use_2to3 to test build jenkins job

--- setup.py
+++ setup.py
@@ -100,8 +100,6 @@ setup(
             "pylint",
         ],
     },
-    # MAYBE-DISABLE: use_2to3
-    use_2to3= bool(python_version >= 3.0),
     license="BSD",
     classifiers=[
         "Development Status :: 4 - Beta",
